94c2c919971f908976b0a03c6db3e35da61456c7
merlinoa <merlinoa88@gmail.com>
2017-11-10 14:35:04 -0500

    added shiny top level page

---
+++ static/js/nav-active.js
@@ -0,0 +1,7 @@
+(function() {
+  // find current page and add active class to nav item
+  let page = window.location.pathname;
+  page = page.replace(/\//g, "");
+  console.log(page);
+  $("#page-" + page).addClass("active");
+})();
